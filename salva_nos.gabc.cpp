7996abb349a2024937cdc1d7b9bd6cbe27080945
Jakub Pavlik <severus@post.cz>
2022-02-08 19:10:51 +0100

    add comment

--- salva_nos.gabc.cpp
+++ salva_nos.gabc.cpp
@@ -6,6 +6,7 @@ transcriber:Andrew Hinkley;
 %%
 (c4) SAl(g)va(hj) nos,(j) Dó(jk)mi(j)ne,(ji) vi(h)gi(i')lán(h)tes,(g.) (;) cu(g)stó(i')di(j) nos(k') dor(j)mi(h')én(j)tes:(i.) (;) ut(i) vi(g')gi(i)lé(k')mus(j) cum(h') Chri(j)sto,(i_j) (,) et(h) re(h)qui(h)e(h)scá(gf)mus(gh) in(gf) pa(e.)ce.(e.) (::)
 
+% alleluia is only included if the TEMPORE_PASCHALI constant is defined
 #ifdef TEMPORE_PASCHALI
 Al(g)le(gf)lú(e.)ia.(e.) (::)
 #endif
